e5c78ad395a3bdd5ef8995cfb3812e3df624a507
Ditte <Ditte@DESKTOP-QORFADN>
2017-10-10 13:27:15 +0200

    Flere projekter og klasser implementeret, Car med properties og UDPReciever med påbegyndt implementering deri

---
+++ PlainUDPReceiver/UDPReceiver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PlainUDPReceiver
+{
+    class UDPReceiver
+    {
+        private readonly int PORT;
+
+        public UDPReceiver(int port)
+        {
+            PORT = port;
+        }
+        public void Start()
+        {
+            using (UdpClient client = new UdpClient(PORT))
+            {
+                byte[] buffer = new byte[2048];
+
+                IPEndPoint SenderEP = new IPEndPoint(IPAddress.Any, 11001);
+
+
+            }
+        }
+    }
+}
